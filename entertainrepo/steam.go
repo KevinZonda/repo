d761ccb1c99bbc4382b6a5820a9ffadc3310716f
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2024-04-08 00:11:18 +0100

    steam telegram

---
+++ entertainrepo/steam.go
@@ -0,0 +1,7 @@
+package entertainrepo
+
+var SteamDl = map[string]string{
+	"win":    "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe",
+	"mac":    "https://cdn.cloudflare.steamstatic.com/client/installer/steam.dmg",
+	"debian": "https://cdn.cloudflare.steamstatic.com/client/installer/steam.dev",
+}
---
+++ entertainrepo/telegram.go
@@ -0,0 +1,13 @@
+package entertainrepo
+
+var TelegramDl = map[string]string{
+	"win64":          "https://telegram.org/dl/desktop/win64",
+	"win64_portable": "https://telegram.org/dl/desktop/win64_portable",
+	"win":            "https://telegram.org/dl/desktop/win",
+	"win_portable":   "https://telegram.org/dl/desktop/win_portable",
+	"mac":            "https://telegram.org/dl/desktop/mac",
+	"mac_store":      "https://telegram.org/dl/desktop/mac_store",
+	"linux":          "https://telegram.org/dl/desktop/linux",
+	"linux_flatpak":  "https://telegram.org/dl/desktop/flatpak",
+	"linux_snap":     "https://telegram.org/dl/desktop/snap",
+}
