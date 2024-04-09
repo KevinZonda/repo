624da625f7a5b076db19a5f1ce371d758b4ea29a
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-04-09 03:57:41 +0100

    fix: go

--- devrepo/go/go.go
+++ devrepo/go/go.go
@@ -49,6 +49,16 @@ type GoIndexItem struct {
 	Files   []GoIndexFile `json:"files"`
 }
 
+func (i GoIndexItem) FileList() GoIndexFileMap {
+	m := map[string]GoIndexFile{}
+	for _, f := range i.Files {
+		m[f.Filename] = f
+	}
+	return m
+}
+
+type GoIndexFileMap map[string]GoIndexFile
+
 type GoIndexFile struct {
 	Filename string `json:"filename"`
 	Os       string `json:"os"`
@@ -59,21 +69,30 @@ type GoIndexFile struct {
 	Kind     string `json:"kind"`
 }
 
+func (m GoIndexFileMap) Url(filename string) string {
+	_, ok := m[filename]
+	if !ok {
+		return ""
+	}
+	return "https://go.dev/dl/" + filename
+}
+
 func (i GoIndexItem) VersionedUrl() repo_standard.VersionedUrl {
+	fl := i.FileList()
 	m := map[repo_standard.Platform]map[repo_standard.Arch]string{
 		repo_standard.PlatformWin: {
-			repo_standard.ArchX64:   dlUrl(i.versionedFilename("windows-amd64")),
-			repo_standard.ArchX86:   dlUrl(i.versionedFilename("windows-386")),
-			repo_standard.ArchARM64: dlUrl(i.versionedFilename("windows-arm64")),
+			repo_standard.ArchX64:   fl.Url(i.versionedFilename("windows-amd64")),
+			repo_standard.ArchX86:   fl.Url(i.versionedFilename("windows-386")),
+			repo_standard.ArchARM64: fl.Url(i.versionedFilename("windows-arm64")),
 		},
 		repo_standard.PlatformMac: {
-			repo_standard.ArchX64:   dlUrl(i.versionedFilename("darwin-amd64")),
-			repo_standard.ArchARM64: dlUrl(i.versionedFilename("darwin-arm64")),
+			repo_standard.ArchX64:   fl.Url(i.versionedFilename("darwin-amd64")),
+			repo_standard.ArchARM64: fl.Url(i.versionedFilename("darwin-arm64")),
 		},
 		repo_standard.PlatformLinux: {
-			repo_standard.ArchX64:   dlUrl(i.versionedFilename("linux-amd64")),
-			repo_standard.ArchX86:   dlUrl(i.versionedFilename("linux-386")),
-			repo_standard.ArchARM64: dlUrl(i.versionedFilename("linux-amd64")),
+			repo_standard.ArchX64:   fl.Url(i.versionedFilename("linux-amd64")),
+			repo_standard.ArchX86:   fl.Url(i.versionedFilename("linux-386")),
+			repo_standard.ArchARM64: fl.Url(i.versionedFilename("linux-amd64")),
 		},
 	}
 	return repo_standard.VersionedUrl{
@@ -90,8 +109,4 @@ func (i GoIndexItem) versionedFilename(arch string) string {
 		ext = ".pkg"
 	}
 	return i.Version + "." + arch + ext
-}
-
-func dlUrl(filename string) string {
-	return "https://go.dev/dl/" + filename
 }
